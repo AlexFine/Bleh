dc2241ddc2575765604b11827a9bfb40a66fd1e2
Ian Loftis <ianloftis@Ians-MacBook-Pro-2.local>
2016-04-26 10:20:20 -0700

    Fixed a crucial bug

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,6 @@
+//Author: Ian Loftis
+//Created: 04/26/16
+
 angular.module('starter.controllers', ['ngCordova', 'jrCrop'])
   .controller('IntroCtrl', function ($scope, $http, $state, $ionicSlideBoxDelegate, $rootScope, $ionicHistory, $stateParams, $ionicLoading) {
 
